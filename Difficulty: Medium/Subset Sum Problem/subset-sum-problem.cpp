c9fdb3299de1340f5fce01b357dd70edd4797e1a
Ayushmaan Joshi <113594168+AyushmaanJoshi@users.noreply.github.com>
2025-03-17 22:46:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size(); // Get array size
+        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
+
+        // Initialize first column as true (sum = 0 is always possible)
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
+
+        // Initialize first row, except dp[0][0], as false (No subset possible)
+        for (int j = 1; j <= sum; j++)
+            dp[0][j] = false;
+
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] > j)
+                    dp[i][j] = dp[i - 1][j]; // Ignore current element
+                else
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]]; // Include or exclude
+            }
+        }
+
+        return dp[n][sum];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+
+        Solution ob;
+        if (ob.isSubsetSum(arr, sum))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
